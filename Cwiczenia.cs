b1fc52193b00f0c0414827451bda3185c44d6aa5
KacperOtreba <114559526+KacperOtreba@users.noreply.github.com>
2022-10-05 09:12:22 +0200

    Add files via upload
    dodanie zrobionych ćwiczeń KartaPracy1 w c#

---
+++ Cwiczenia.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*a + Math.Pow(b,2));
+            */
+
+            // zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine((a+b) * (a+b));
+            */
+
+            // zad 3
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow((a - b), 3));
+            */
+
+            // zad 4
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b * c);
+            Console.ReadKey();
+            */
+
+            //zad 5
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(2 * (a + b) / 5);
+            Console.ReadKey();
+            */
+
+            //zad 6
+            /*
+            int brutto = int.Parse(Console.ReadLine());
+            Console.WriteLine(brutto/1.23);
+            Console.ReadKey();
+            */
+
+            //zad7
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a % b);
+            Console.ReadKey();
+        }
+
+    }
+}
