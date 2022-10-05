927ca16bc37f1b0074b5b45f5b6004709704c3cf
KacperOtreba <114559526+KacperOtreba@users.noreply.github.com>
2022-10-05 09:35:13 +0200

    dodanie zrobionych ćwiczeń

---
+++ KartaPracy2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            Console.ReadKey();
+            */
+
+            //zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a > 99 && a < 1000 && a % 17 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            Console.ReadKey();
+            */
+
+            //zad 3
+            /*
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek >= 18)
+            {
+                Console.WriteLine("GIT");
+            }
+            else
+            {
+                Console.WriteLine("NIE GIT");
+            }
+            Console.ReadKey();
+            */
+            
+            //zad 4
+            int LIMIT 
+
+
+
+
+
+
+
+        }
+    }
+}
